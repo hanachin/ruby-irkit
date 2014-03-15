899c068e1edf293359e1aafca17ec17dd842a04f
Sho Hashimoto <hashimoto@shokai.org>
2014-03-15 22:23:52 +0900

    set bonjour-name into IRKit::Device

--- lib/irkit/find.rb
+++ lib/irkit/find.rb
@@ -7,7 +7,7 @@ module IRKit
       DNSSD.browse '_irkit._tcp' do |reply|
         next unless reply.name =~ /irkit/
         addrs = Socket.getaddrinfo("#{reply.name}.local.", nil, Socket::AF_INET)
-        hosts.push IRKit::Device.new(addrs[0][2]) rescue next
+        hosts.push IRKit::Device.new(addrs[0][2], reply.name) rescue next
       end
       5.times do
         sleep 1
